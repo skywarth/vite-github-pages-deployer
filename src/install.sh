25168b899bdd779aa24bb34c652ca17f857805bc
skywarth <yigitk.ersoy@gmail.com>
2023-05-31 00:16:27 +0300

    bugfix: correct access syntax

--- src/install.sh
+++ src/install.sh
@@ -1,8 +1,8 @@
-if [[ "${{ PACKAGE_MANAGER }}" == "yarn" ]]
+if [[ "$PACKAGE_MANAGER" == "yarn" ]]
 then
-  if [[ "${{ DEBUG_MODE }}" == "true" ]]; then echo "Installing via yarn"; fi;
+  if [[ "$DEBUG_MODE" == "true" ]]; then echo "Installing via yarn"; fi;
   yarn install --immutable --immutable-cache --check-cache;
 else
-  if [[ "${{ DEBUG_MODE }}" == "true" ]]; then echo "Installing via npm"; fi;
+  if [[ "$DEBUG_MODE" == "true" ]]; then echo "Installing via npm"; fi;
   npm ci;
 fi

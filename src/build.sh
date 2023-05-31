5d973d2ad9c562d0d3258f927b441c6e85e25b49
skywarth <yigitk.ersoy@gmail.com>
2023-05-31 23:32:15 +0300

    feat: move build scripts to sh file

---
+++ src/build.sh
@@ -0,0 +1,8 @@
+if [[ "$PACKAGE_MANAGER" == "yarn" ]]
+then
+  if [[ "$DEBUG_MODE" == "true" ]]; then echo "Building via yarn"; fi;
+  yarn build -- --base="$PUBLIC_BASE_PATH"
+else
+  if [[ "$DEBUG_MODE" == "true" ]]; then echo "Building via npm"; fi;
+  npm run build -- --base="$PUBLIC_BASE_PATH"
+fi
